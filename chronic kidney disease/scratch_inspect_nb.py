4a25135955f11e64eaf8b4579f907db1a568257b
alfaturachman <122202202929@mhs.dinus.ac.id>
2026-07-24 00:25:48 +0700

    Remove failed projects (breast cancer & chronic kidney disease) from repository and documentation

--- chronic kidney disease/scratch_inspect_nb.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("Kidney_Disease.ipynb", "r", encoding="utf-8") as f:
-    nb = json.load(f)
-
-print("Total cells:", len(nb["cells"]))
-for i, cell in enumerate(nb["cells"]):
-    src = "".join(cell.get("source", []))[:100].replace("\n", " ")
-    print(f'{i:02d}: {cell["cell_type"]:8s} | {src}')
--- chronic kidney disease/scratch_print_cell22.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("Kidney_Disease.ipynb", "r", encoding="utf-8") as f:
-    nb = json.load(f)
-
-print("=== Cell 22 Source ===")
-print("".join(nb["cells"][22]["source"]))
